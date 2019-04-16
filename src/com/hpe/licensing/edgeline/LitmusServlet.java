bf961141bdf4736d2fb0ab674cc8c0529aa780a4
Francois Tiercelin <francois.tiercelin@hpe.com>
2019-04-16 13:37:38 +0000

    Workload Orchestrator help text

--- src/com/hpe/licensing/edgeline/LitmusServlet.java
+++ src/com/hpe/licensing/edgeline/LitmusServlet.java
@@ -42,9 +42,10 @@ public class LitmusServlet extends HttpServlet {
 	private static final String ACTIV_KEY_REGEX = "^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$";
 
 
-	private static final String HELP_ACTIV_KEY = "The activation key has been provided to you when registering your product.<br/>It is formatted as &lt;4 digits&gt;-&lt;4 digits&gt;-&lt;4 digits&gt;-&lt;4 digits&gt;";
+	private static final String HELP_ACTIV_KEY = "The activation key has been provided to you when registering your product."
+			+ "<br/>It is formatted as &lt;4 digits&gt;-&lt;4 digits&gt;-&lt;4 digits&gt;-&lt;4 digits&gt;";
 	private static final String HELP_HOST_ID = "For OT Link Platform, the Host ID can be found on the License screen under the System Option menu."
-			+ "<br/>For Workload Orchestrator, type hosted at the command prompt on the system where Workload Orchestrator is running.";
+			+ "<br/>For Workload Orchestrator, the Host ID can be found by typing &quot;hosted&quot; at the command prompt on the system where Workload Orchestrator is running.";
 
 	
 	private static final String HTML_JAVASCRIPT = "./ot.js.html";
