208d48668dd96eb7bcc329c5ab50c1cbcd175782
Francois Tiercelin <francois.tiercelin@hpe.com>
2019-04-19 05:59:42 +0000

    another missing Edgeline

--- src/com/hpe/licensing/edgeline/LitmusServlet.java
+++ src/com/hpe/licensing/edgeline/LitmusServlet.java
@@ -51,7 +51,7 @@ public class LitmusServlet extends HttpServlet {
 	private static final String HELP_ACTIV_KEY = "The activation key has been provided to you when registering your product."
 			+ "<br/>It is formatted as &lt;4 digits&gt;-&lt;4 digits&gt;-&lt;4 digits&gt;-&lt;4 digits&gt;";
 	private static final String HELP_HOST_ID = "For Edgeline OT Link Platform, the Host ID can be found on the License screen under the System Option menu."
-			+ "<br/>For Edgeline Workload Orchestrator, the Host ID can be found by typing &quot;hosted&quot; at the command prompt on the system where Workload Orchestrator is running.";
+			+ "<br/>For Edgeline Workload Orchestrator, the Host ID can be found by typing &quot;hosted&quot; at the command prompt on the system where Edgeline Workload Orchestrator is running.";
 
 	
 	private static final String HTML_JAVASCRIPT = "./ot.js.html";
